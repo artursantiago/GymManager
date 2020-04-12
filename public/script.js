35db8f1a291639d4468bbb87e5410b16a4268aab
Artur Santiago <artursantiago2001@hotmail.com>
2020-04-12 13:41:18 -0300

    fix formDelete error

--- public/script.js
+++ public/script.js
@@ -2,16 +2,19 @@ const currentPage = location.pathname;
 
 const menuItems = document.querySelectorAll('header .links a');
 
-for (item of menuItems)  {
-    if (currentPage.includes(item.getAttribute('href'))) {
-        item.classList.add('active');
-    }
+for (item of menuItems) {
+  if (currentPage.includes(item.getAttribute('href'))) {
+    item.classList.add('active');
+  }
 }
 
 const formDelete = document.querySelector('.form-delete');
-formDelete.addEventListener('submit', event => {
+
+if (formDelete) {
+  formDelete.addEventListener('submit', event => {
     if (!confirm('Do you want to delete?')) {
-        event.preventDefault();
+      event.preventDefault();
     }
-});
+  });
+}
 
